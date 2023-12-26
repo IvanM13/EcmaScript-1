f509fdc54e2b7fc90ac8ca43757862ec079c65dd
Ivan M <slade91@mail.ru>
2023-12-26 16:53:56 +0300

    add const2

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ console.log(Math.min(...arr));
 
 // 2) Напишите функцию createCounter, которая создает счетчик и возвращает объект с двумя методами: increment и decrement. Метод increment должен увеличивать значение счетчика на 1, а метод decrement должен уменьшать значение счетчика на 1. Значение счетчика должно быть доступно только через методы объекта, а не напрямую.
 
-createCounter = () => {
+const createCounter = () => {
     let count = 0;
     return {
       increment: () => {
